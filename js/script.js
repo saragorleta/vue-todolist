6208c89a559334be82a25f1144c47ff15d54e0a9
Sara <saragorleta@yahoo.com>
2021-01-28 20:17:23 +0100

    inserito js con prova del bonus per eliminare tutto

--- js/script.js
+++ js/script.js
@@ -1,19 +1,44 @@
 var app= new Vue({
   el: '#app',
   data:{
+
+    newTodo:'',
+
     todos:[
       'fare gli esercizi',
       'andare in palestra',
       'fare la spesa',
       'cucinare',
       'andare a camminare'
-    ]
-  }
-,
+    ],
+
+    todosEliminati:[],
+
+    todosfinalElimination:[]
+  },
+
 methods: {
-  // Funzione per eliminare
+  addTodo(){
+  this.todos.push(this.newTodo);
+  this.newTodo='';  //per svuotare la casella di input
+},
+
+  // FUNZIONE PER ELIMINARE E AGGIUNGERE ALLA LISTA DI ELEMENTI ELIMINATI
   deleteTodo(index){          //creo funzione, richiamo l'indice
-  this.todos.splice(index,1);  //e gli dico che a todos deve eliminare l'indice 1 sta a indicare un elemento per volta
-  }
+  this.todosEliminati.push(this.todos[index]);  //pushamo nell'array vuoto gli elementi eliminati
+  this.todos.splice(index,1); //e gli dico che a todos deve eliminare l'indice 1 sta a indicare un elemento per volta
+  // console.log(this.todosEliminati);
+},
+// RIPRISTINO ELEMENTI
+  restoreTodos(index){
+  this.todos.push(this.todosEliminati[index]) //prendiamo elemento da todosEliminati e lo pushamo all'interno di todos
+  this.todosEliminati.splice(index,1);
+},
+// ELIMINAZIONE DEFINITIVA
+finalElimination(index){
+this.todosEliminati.push(this.todosfinalElimination[index]);
+this.todosfinalElimination.splice(index,1);
 }
+}
+
 });
