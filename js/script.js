b4b36a080516a4a544b6ac785bb9cadba3dcab30
Sara <saragorleta@yahoo.com>
2021-01-28 16:32:44 +0100

    add html/css

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var app= new Vue({
+  el: '#app',
+  data:{
+    todos:[
+      'fare gli esercizi',
+      'andare in palestra',
+      'fare la spesa',
+      'cucinare',
+      'andare a camminare'
+    ]
+  }
+});
